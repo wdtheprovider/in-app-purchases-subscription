405355b5fd57d12800d601fcd618b15a3c846d01
Dingaan Letjane <velly.dingaan17@gmail.com>
2022-01-21 20:43:46 +0200

    Initial commit

---
+++ app/src/main/java/com/wdtheprovider/sharcourse/StoreActivity.java
@@ -0,0 +1,14 @@
+package com.wdtheprovider.sharcourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
