598dfe12082ac631dd37e8d123dc58adb8970832
Dingaan Letjane <velly.dingaan17@gmail.com>
2022-01-22 12:27:38 +0200

    message

--- app/src/main/java/com/wdtheprovider/sharcourse/MainActivity.java
+++ app/src/main/java/com/wdtheprovider/sharcourse/MainActivity.java
@@ -2,13 +2,29 @@ package com.wdtheprovider.sharcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_subscribe;
+    TextView txt_subscribed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        //Opening the Store activity.
+        btn_subscribe.setOnClickListener(view -> startActivity(new Intent(this,StoreActivity.class)));
+    }
+
+    private void initViews() {
+
+        btn_subscribe= findViewById(R.id.btn_subscribe);
+        txt_subscribed= findViewById(R.id.txt_subscribed);
     }
 }
--- app/src/main/java/com/wdtheprovider/sharcourse/SplashActivity.java
+++ app/src/main/java/com/wdtheprovider/sharcourse/SplashActivity.java
@@ -2,13 +2,31 @@ package com.wdtheprovider.sharcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        checkSubscription();
+
+        handler.postDelayed(this::viewMainActivity,2000);
+    }
+
+    private void checkSubscription() {
+        //checking if there is a subscription active for logged on
+        //gmail account on play store
+    }
+
+    void viewMainActivity(){
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
